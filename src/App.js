9860adb289afe0e2d639689719385daf73e6ba60
Fa-v <fabiolacvieyra@gmail.com>
2017-07-13 19:51:46 +0200

    adding players and winner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import './index.css';
 
 function Square(props) {
@@ -11,11 +12,11 @@ function Square(props) {
 }
 
 class Board extends React.Component{
-  renderSquare(i) {
+  renderSquare(squareIndex) {
     return (
       <Square
-        squares={this.props.squares[i]}  
-        onClick={() => this.props.onClick(i)}
+        squares={this.props.squares[squareIndex]}  
+        onClick={() => this.props.onClick(squareIndex)}
       />
     );
   }
@@ -49,33 +50,85 @@ class App extends Component {
 
     this.state = {
       squares: Array(9).fill(null),
-      firstPlayer: 'X',
-      nextMove: false
+      xPlayer: true,
+      moves: [],
+      result: null
     }
   }
 
-  handleClick(i) {
-    let squares = this.state.squares;
-    squares[i] = this.state.firstPlayer; 
-    console.log(squares)
+  handleClick(squareIndex) {
+    const { squares, xPlayer, moves } = this.state;
+    const squaresCopy = squares.slice();
+    const updatedMoves = moves.concat(squareIndex);
+    const winnerResult = this.winner(updatedMoves);
+    squaresCopy[squareIndex] = xPlayer ? 'X' : 'O';
+
+    if (squares[squareIndex] !== null) {
+      return;
+    }
+
     this.setState({
-      squares: squares
-    })
+      squares: squaresCopy,
+      xPlayer: !this.state.xPlayer,
+      moves: updatedMoves,
+      result: winnerResult
+    });
   }
 
+  winner(moves) {
+    const lineCompleted = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ];
+    const xMoves = [];
+    const oMoves = [];
+      
+    moves.filter((move, index) => {
+      return index % 2 === 0 ? xMoves.push(move) : oMoves.push(move);
+    });
+
+    const result = lineCompleted.map((line) => {
+      const [a, b, c] = line;
+      
+      return (xMoves.includes(a) && xMoves.includes(b) && xMoves.includes(c))
+        ? 'X' : (oMoves.includes(a) && oMoves.includes(b) && oMoves.includes(c))
+          ? 'O' : null;
+      
+    }); 
+
+    return result.includes('X') ? 'X' : result.includes('O') ? 'O' : null;
+  }
+
+  resetBoard() {
+    this.setState({
+      squares: Array(9).fill(null),
+      xPlayer: true,
+      moves: [],
+      result: null
+    })
+  }  
+
   render() {
+    const { xPlayer, squares, result } = this.state;
     return (
       <div className="game">
         <div className="board">
           <Board
-            squares={this.state.squares}
-            onClick={i => this.handleClick(i)}/>
+            squares={squares}
+            onClick={(squareIndex) => this.handleClick(squareIndex)} />
+          <p>{xPlayer ? 'X\'s turn' : 'O\'s turn'}</p>
+          <p>{_.isNull(result) ? '' : 'The winner is: ' + result}</p>
+          <button onClick={() => this.resetBoard()}>{'Reset'}</button>
         </div>
       </div>
     );
   }
-
-
 }
 
 export default App;
